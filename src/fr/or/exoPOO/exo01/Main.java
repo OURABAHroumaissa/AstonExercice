c246ceef84c79b4019f0e0c226a390e14d988875
roumaissa OURABAH <er_ourabah@esi.dz>
2022-05-13 09:09:30 +0200

    exercice poo

--- src/fr/or/exoPOO/exo01/Main.java
+++ src/fr/or/exoPOO/exo01/Main.java
@@ -1,11 +1,13 @@
-package fr.exo01;
+package fr.or.exoPOO.exo01;
+
+
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle01 = new Rectangle();
+      /*  Rectangle rectangle01 = new Rectangle();
 
         // Instancier l'objet rectancle02 de type Rectangle
-        Rectangle rectangle02 = new Rectangle(10, 30);
+        fr.exo01.Rectangle rectangle02 = new Rectangle(10, 30);
 
         // Modifier la valeur de la largeur initialisée à 10
         rectangle02.largeur = 8;
@@ -13,6 +15,6 @@ public class Main {
         double surface = rectangle02.calculerSurface();
 
         // Afficher la surface
-        System.out.println("La surface est de : " + surface);
+        System.out.println("La surface est de : " + surface);*/
     }
 }
